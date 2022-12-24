643733b63b2ffb2fdcaf95148b129749afe2732c
Justin <justinatteberry8@gmail.com>
2022-12-24 15:47:48 -0500

    File formatting updated and placed some "if" statements depending on shape and axis of bending.

--- main.py
+++ main.py
@@ -4,34 +4,45 @@ import flexure.flexure_f as flexure_check
 
 """All user inputs should be converted to inches and kips"""
 
+"""Ask user what they want to do"""
+user_input = input("What design do you want to check? (Beam, Column, Tension)\n").lower()
+
 """Call lookup function to get the starting shapes properties"""
 shape_properties = lookup()
-beam_length = int(input("Length of beam (ft): ")) * 12
-unbraced_length = int(input("Maximum unbraced segment of beam (ft): ")) * 12
+shape = shape_properties["Type"]
 
+if user_input == "beam":
+	axis = input("Is bending about major axis? (Y/N)").lower()
+	beam_length = int(input("Length of beam (ft): ")) * 12
+	unbraced_length = int(input("Maximum unbraced segment of beam (ft): ")) * 12
 
-"""Check f2 flexural capacity"""
-f2_yielding = flexure_check.f2_yielding(
-	fy=sp.yield_stress,
-	zx=shape_properties["Zx"]
-)
+	if axis == "y":  # Specifies bending about major axis
+		if shape == "W" or shape == "S" or shape == "M" or shape == "C" or shape == "MC":
+			"""Check f2 flexural capacity"""
+			f2_yielding = flexure_check.f2_yielding(
+				fy=sp.yield_stress,
+				zx=shape_properties["Zx"]
+			)
 
-print(f2_yielding)
+			print(f2_yielding)
 
-"""Check f3 flexural capacity"""
-f2_ltb = flexure_check.f2_ltb(
-	shape=shape_properties["Type"].lower(),
-	lb=unbraced_length,
-	ry=shape_properties["ry"],
-	fy=sp.yield_stress,
-	e=sp.modulus_of_elasticity,
-	rts=shape_properties["rts"],
-	iy=shape_properties["Iy"],
-	cw=shape_properties["Cw"],
-	j=shape_properties["J"],
-	sx=shape_properties["Sx"],
-	ho=shape_properties["ho"],
-	cb=1.14
-)
+			"""Check f3 flexural capacity"""
+			f2_ltb = flexure_check.f2_ltb(
+				shape=shape_properties["Type"].lower(),
+				lb=unbraced_length,
+				ry=shape_properties["ry"],
+				fy=sp.yield_stress,
+				e=sp.modulus_of_elasticity,
+				rts=shape_properties["rts"],
+				iy=shape_properties["Iy"],
+				cw=shape_properties["Cw"],
+				j=shape_properties["J"],
+				sx=shape_properties["Sx"],
+				ho=shape_properties["ho"],
+				cb=1.14
+			)
 
-print(f2_ltb)
+			print(f2_ltb)
+
+# TODO add an exception for the shapes called out in the "user note" in section F2 for shapes with non-compact flanges
+# This is an F3 check
--- shape_lookup/shape_lookup.py
+++ shape_lookup/shape_lookup.py
@@ -11,12 +11,31 @@ def lookup():
 		shape_df.set_index("AISC_Manual_Label", inplace=True)
 
 		"""Ask user for input information on steel beam"""
-		shape = input("Beam Size (wXXxXXX): ").upper()
+		shape = input(
+			"Shapes:"
+			"\nW-Shape --> wXXxXXX"
+			"\nM-Shape --> mXXxXX"
+			"\nS-Shape --> sXXxXX"
+			"\nHP-Shape --> hpXXxXXX"
+			"\nChannels --> cXXxXX"
+			"\nChannels --> mcXXxXX"
+			"\nAngles --> lXxXxX-X/X"
+			"\nWT-Shape --> wtXXxXX"
+			"\nMT-Shape --> mtXXxXX"
+			"\nST-Shape --> stXXxXX"
+			"\nDouble Angles --> 2lXXxXXxX-X/X(LLBB/SLBB)"
+			"\nHollow Structural Section (Square/Rectangular) --> hssXXxXXxX/X"
+			"\nHollow Structural Section (Round) --> hssXX.XXXxX.XXX"
+			"\nPipe --> pipeX-X/X(std/xs/xxs)\n"
+			"What Shape would you like to Check? --> "
+		).upper()
+
 		"""Utilize .loc method to search for specific shape"""
 		try:
 			shape_properties = shape_df.loc[shape].to_dict()
 		except KeyError:
 			print("Invalid Beam Size. Try again.")
+			lookup()
 		else:
 			break
 	return shape_properties
